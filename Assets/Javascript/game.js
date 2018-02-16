45113831fb3d8ee890dc093cbb40fc4905b41b57
Shamil Arefin <landoflitany@gmail.com>
2018-02-15 18:29:17 -0600

    Commit

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,24 @@
+var words = [
+    "crybaby",
+    "dollhouse",
+    "sippycup",
+    "carousel",
+    "alphabetboy",
+    "soap",
+    "trainingwheels",
+    "pityparty",
+    "tagyoureit",
+    "milkandcookies",
+    "pacifyher",
+    "mrspotatohead",
+    "madhatter"
+];
+var correctInput = "";
+var wrongInput = "";
+var wins = 0;
+var losses = 0;
+var guesses = 0;
+var alreadyguessed = [];
+document.onkeyup(){
+    
+}
